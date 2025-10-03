1845fa0b83ab2ecb59f4378e7e8f04fe6a2360f1
Erertertet <s2293297@ed.ac.uk>
2025-10-03 18:44:26 +0100

    feat: auto update from github
    refactor: simplified code and fixed issue #2
    
    hotfix: console log
    
    hotfix: changed the order of classifier
    
    feat:change color code to oklch
    
    fix: hide time despite didn't insert new one on top
    
    format: when did I put in that space
    
    fix:delete /

---
+++ main.meta.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         better_zhihu
+// @namespace    https://github.com/erertertet/betterZhihu
+// @version      1.3.5
+// @description  在知乎回答和文章中标记评论/点赞比，将编辑时间和发布时间显示在标题下方，隐藏原始时间，优化分享和按钮布局
+// @author       Erertertet
+// @match        https://www.zhihu.com/*
+// @downloadURL  https://github.com/erertertet/betterZhihu/blob/main/main.user.js?raw=true
+// @updateURL    https://github.com/erertertet/betterZhihu/blob/main/main.meta.js?raw=true
+// @grant        none
+// ==/UserScript==
