676ee091f70571fae1094842a17400c2422edf3d
Erertertet <s2293297@ed.ac.uk>
2025-10-03 21:00:24 +0100

    hotfix: console log

--- main.user.js
+++ main.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         better_zhihu
 // @namespace    https://github.com/erertertet/betterZhihu
-// @version      1.3.3
+// @version      1.3.4
 // @description  在知乎回答和文章中标记评论/点赞比，将编辑时间和发布时间显示在标题下方，隐藏原始时间，优化分享和按钮布局
 // @author       Erertertet
 // @match        https://www.zhihu.com/*
@@ -81,7 +81,7 @@
 
         const jsonDataCombined = { ...jsonData, ...jsonExtra };
 
-        console.log(jsonDataCombined);
+        // console.log(jsonDataCombined);
 
         let title = jsonDataCombined.title;
         let author = jsonDataCombined.authorName;
