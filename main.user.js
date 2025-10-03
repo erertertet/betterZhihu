c8454209ca72cb024dbb8ba77623472294218cbf
Erertertet <s2293297@ed.ac.uk>
2025-10-03 18:44:26 +0100

    feat: auto update from github

--- main.user.js
+++ main.user.js
@@ -1,10 +1,12 @@
 // ==UserScript==
 // @name         better_zhihu
-// @namespace    http://tampermonkey.net/
-// @version      1.3.1
+// @namespace    https://github.com/erertertet/betterZhihu
+// @version      1.3.2
 // @description  在知乎回答和文章中标记评论/点赞比，将编辑时间和发布时间显示在标题下方，隐藏原始时间，优化分享和按钮布局
 // @author       Erertertet
 // @match        https://www.zhihu.com/*
+// @downloadURL  https://github.com/erertertet/betterZhihu/blob/main/main.user.js?raw=true
+// @updateURL    https://github.com/erertertet/betterZhihu/blob/main/main.meta.js?raw=true
 // @grant        none
 // ==/UserScript==
 
