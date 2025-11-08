f2ad010459da1141705abfd209ddf78c74739ca8
erertertet <41342153+erertertet@users.noreply.github.com>
2025-11-08 00:50:22 +0000

    hotfix: Bump version to 1.5.0 and update description
    Updated version and description to include text copy feature.

--- main.meta.js
+++ main.meta.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         better_zhihu
 // @namespace    https://github.com/erertertet/betterZhihu
-// @version      1.4.0
-// @description  在知乎回答和文章中标记评论/点赞比，将编辑时间和发布时间显示在标题下方，隐藏原始时间，优化分享和按钮布局
+// @version      1.5.0
+// @description  在知乎回答和文章中标记评论/点赞比，将编辑时间和发布时间显示在标题下方，隐藏原始时间，优化分享和按钮布局，启用文本复制
 // @author       Erertertet
 // @match        https://www.zhihu.com/*
 // @downloadURL  https://github.com/erertertet/betterZhihu/blob/main/main.user.js?raw=true
